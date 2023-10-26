a1a32714c14da1696fdf82cc9d43c0ec32659ae8
Ezequiel <ezequielresipa45@gmail.com>
2023-10-25 22:23:52 -0300

    Changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-
-import './App.css'
+import "./App.css";
+import ilustration from "../../assets/images/illustration-sign-up-desktop.svg";
 
 function App() {
- 
-
   return (
-    <>
-
-
-    </>
-  )
+    <div className="container-body">
+      <div className="container-newslatter">
+        <div className="container-text">
+          Stay updated! Join 60,000+ product managers receiving monthly updates
+          on: Product discovery and building what matters Measuring to ensure
+          updates are a success And much more! Email address email@company.com
+          Subscribe to monthly newsletter
+        </div>
+        <img className="ilustration" src={ilustration} alt="ilustration" />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
